b802d8026446f86fdb2b00b16c1fc427bc9ab313
wakeait <wakeait@gmail.com>
2019-08-20 11:57:54 +0800

    modify

--- switch/switch.ino
+++ switch/switch.ino
@@ -10,9 +10,9 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available()>0){
     char rx = Serial.read();
-    if(rx=='1'){digitalWrite(13,HIGH);}
+    if(rx=='1'){digitalWrite(13,HIGH);digitalWrite(12,LOW);digitalWrite(7,LOW);}
     else if (rx=='0'){digitalWrite(13,LOW);digitalWrite(12,LOW);digitalWrite(7,LOW);}
-    else if (rx=='2'){digitalWrite(13,HIGH); digitalWrite(12,HIGH);}
+    else if (rx=='2'){digitalWrite(13,HIGH); digitalWrite(12,HIGH);digitalWrite(7,LOW);}
     else if (rx=='3'){digitalWrite(13,HIGH); digitalWrite(12,HIGH);digitalWrite(7,HIGH);}
   }
 }

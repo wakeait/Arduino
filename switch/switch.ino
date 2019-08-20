4944cbc184559a2cd125fef3222e4a0b6a06c9f8
wakeait <wakeait@gmail.com>
2019-08-20 11:48:02 +0800

    3 led

--- switch/switch.ino
+++ switch/switch.ino
@@ -1,6 +1,8 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(7,OUTPUT);
   Serial.begin(115200);
 }
 
@@ -9,6 +11,8 @@ void loop() {
   if (Serial.available()>0){
     char rx = Serial.read();
     if(rx=='1'){digitalWrite(13,HIGH);}
-    else if (rx=='0'){digitalWrite(13,LOW);}
+    else if (rx=='0'){digitalWrite(13,LOW);digitalWrite(12,LOW);digitalWrite(7,LOW);}
+    else if (rx=='2'){digitalWrite(13,HIGH); digitalWrite(12,HIGH);}
+    else if (rx=='3'){digitalWrite(13,HIGH); digitalWrite(12,HIGH);digitalWrite(7,HIGH);}
   }
 }

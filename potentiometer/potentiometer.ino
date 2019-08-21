01ff193981f8d300201c490651f8213e41c5c4e5
wakeait <wakeait@gmail.com>
2019-08-21 11:44:10 +0800

    function

--- potentiometer/potentiometer.ino
+++ potentiometer/potentiometer.ino
@@ -1,5 +1,6 @@
 #define redPin 6
 #define potPin A0
+unsigned long noteTime = millis();
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -8,9 +9,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int anV = analogRead(potPin);
-  int valueof8bit = (float)anV/1023*255;
-  analogWrite(redPin,valueof8bit);
-  Serial.println(valueof8bit);
-  delay(100);
+  doSomething();
+}
+
+void doSomething(){
+  unsigned long currentTime = millis();
+  if((currentTime - noteTime)>=100){
+    noteTime = currentTime;
+    int anV = analogRead(potPin);
+    int valueof8bit = (float)anV/1023*255;
+    analogWrite(redPin,valueof8bit);
+    Serial.println(valueof8bit);
+  }
 }

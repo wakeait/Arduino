b2bc96d3b137de6e7952bfe8714567ee88e48832
wakeait <wakeait@gmail.com>
2019-08-23 10:28:06 +0800

    Led on

--- LedClass/LedClass.ino
+++ LedClass/LedClass.ino
@@ -4,6 +4,16 @@ class Led{
   public:
     Led(byte pin){
       _pinNum = pin;
+      pinMode(_pinNum,OUTPUT);
+    }
+    byte getPinNum(){
+      return _pinNum;
+    }
+    void on(){
+      digitalWrite(_pinNum,HIGH);
+    }
+    void off(){
+      digitalWrite(_pinNum,LOW);
     }
 };
 
@@ -12,7 +22,11 @@ Led secondLed(12);
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  Serial.println(firstLed.getPinNum());
+  Serial.println(secondLed.getPinNum());
+  firstLed.on();
+  secondLed.on();
 }
 
 void loop() {

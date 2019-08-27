9a60af0ca8df645548755318f1387b785c1e5436
wakeait <wakeait@gmail.com>
2019-08-27 13:04:52 +0800

    modify

--- double_button/double_button.ino
+++ double_button/double_button.ino
@@ -12,46 +12,61 @@ void setup() {
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  unsigned long leftCounts = leftBtn.getStateChangeCount();  
-  Serial.print("leftCount:");
-  Serial.println(leftCounts);
-  //製作left2段式
+  unsigned long leftCounts = leftBtn.getStateChangeCount();
+  unsigned long rightCounts = rightBtn.getStateChangeCount();  
   if (leftCounts>0){
-  switch (leftCounts % 4){
-    case 2:
-      //click一次時
-      Serial.println("left1");
-      redLed.on();
-      yellowLed.on();
+  switch (leftCounts % 8){
+    case 0:
+      redLed.off();
+      yellowLed.off();
       greenLed.off();
       break;
-    case 0:
-      //click2次時
-      Serial.println("left2");
-      redLed.off();
-      yellowLed.on();
-      greenLed.on();
-      break;     
-  }
-  }
-  unsigned long rightCounts = rightBtn.getStateChangeCount();
-  Serial.print("rightCount:");
-  Serial.println(rightCounts);
-  //製作right3段式
-  switch (rightCounts % 6){
     case 2:
-      //click一次時
-      Serial.println("right1");
-     
+      switch (rightCounts % 6){
+        case 0:
+          redLed.on();
+          yellowLed.off();
+          greenLed.off();
+          break;
+        case 2:
+          redLed.off();
+          yellowLed.on();
+          greenLed.off();
+          break;
+        case 4:
+          redLed.off();
+          yellowLed.off();
+          greenLed.on();
+          break;
+      }
       break;
     case 4:
-      //click2次時
-       Serial.println("right2");
-      break;
-    case 0:
-      //click3次時
-       Serial.println("right3");
-      break;     
+       if (rightCounts % 6 == 0){
+          redLed.on();
+          yellowLed.on();
+          greenLed.off();
+          break;
+       }
+       else if (rightCounts % 6 == 2){
+          redLed.on();
+          yellowLed.off();
+          greenLed.on();
+          break;
+        }
+       else {
+          redLed.off();
+          yellowLed.on();
+          greenLed.on();
+          break;
+        }
+        break;
+    case 6:
+       redLed.on();
+       yellowLed.on();
+       greenLed.on();
+       break;
+       
   }
-  delay(100);
+  }
+  
 }

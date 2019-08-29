68b0d86a6dc33b99a6c959306450f3a67cf7dcbd
wakeait <wakeait@gmail.com>
2019-08-29 12:50:17 +0800

    modify

--- seven_segmented_for_class/seven_segmented_for_class.ino
+++ seven_segmented_for_class/seven_segmented_for_class.ino
@@ -1,16 +1,20 @@
 #include "SevenSegmented.h"
-
+#include "SwitchButton.h"
 byte mapArdiuinoPin[] = {2, 3, 4, 5, 6, 7, 8};
 SevenSegmented s1(mapArdiuinoPin);
+SwitchButton rightBtn(12);
 void setup() {
   // put your setup code here, to run once:
-  s1.displayDigit(7);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  unsigned long rightCounts = rightBtn.getStateChangeCount();
+  unsigned long getDigit = rightCounts%4;
+  if(getDigit==0){
   for(int i=9;i>=0;i--){
   s1.displayDigit(i);
   delay(50);
  }
+  }
 }

b8f343671d9577be8001b16d79269a0c91613284
wakeait <wakeait@gmail.com>
2019-09-09 13:47:22 +0800

    7seg

--- esp8266_7_segmented/esp8266_7_segmented.ino
+++ esp8266_7_segmented/esp8266_7_segmented.ino
@@ -1,10 +1,11 @@
-#define dataPin 2
-#define latchPin 3
-#define clockPin 4
-#define inputPin A0
 #include "FirebaseESP8266.h"
 #include <ESP8266WiFi.h>
-byte index = 0;
+
+FirebaseData firebaseData;
+#define dataPin D2
+#define latchPin D3
+#define clockPin D4
+
 const byte LEDs[10] = {
   B01111110, //0
   B00110000, //1
@@ -17,21 +18,40 @@ const byte LEDs[10] = {
   B01111111, //8
   B01110011  //9
 };
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  WiFi.begin("robert_hsu", "1234567890");
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
 
-void setup() {
+  Firebase.begin("iotwilly.firebaseio.com", "O4sQ98sdnxVyKmts0ZQ6VG8LtQIcChnCqvBgDW05");
+  Firebase.reconnectWiFi(true);
+  Firebase.setMaxRetry(firebaseData, 3);
+  Firebase.setMaxErrorQueue(firebaseData, 30);
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
-  pinMode(inputPin, INPUT);
-  Serial.begin(115200);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  index = firebaseData.intData();
-  Serial.println(index);
-  //digitalWrite(latchPin, LOW);
-  //shiftOut(dataPin, clockPin,LSBFIRST,LEDs[index]);
-  //digitalWrite(latchPin, HIGH);
-  delay(500);
+  if (Firebase.getInt(firebaseData, "/iot0624/sevenSeg")) {
+    if (firebaseData.dataType() == "int") {
+      int num = firebaseData.intData();
+      digitalWrite(latchPin, LOW);
+      shiftOut(dataPin, clockPin,LSBFIRST,LEDs[num]);
+      digitalWrite(latchPin, HIGH);
+      Serial.println(firebaseData.intData());
+    }
+  } else {
+    Serial.println(firebaseData.errorReason());
+  }
+  //delay(500);
 }
